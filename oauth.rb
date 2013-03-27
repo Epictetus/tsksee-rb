e0cfcbe491f971d03cbdd3a0c7f42e92d001ed8e
k <ksswre@yahoo.co.jp>
2013-03-27 19:14:07 +0900

    mistake

--- oauth.rb
+++
@@ -1,7 +0,0 @@
-require 'rubygems' 
-require 'Twitter' 
-consumer_token = 'XHC32mRQQg4d2A3Gb2xSFA' 
-consumer_secret = 'HPsMqbPU0yWWxh5vBXqKP17qowgKoeecTtXsrXvc' 
-oauth = Twitter::OAuth.new(consumer_token, consumer_secret) 
-request = oauth.consumer.get_request_token 
-puts request.authrize_url #<- ここで認証URLを表示させる 
